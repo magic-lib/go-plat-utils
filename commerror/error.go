b5f9fe98beb10ad4b31aa0973ded4d4d335a43a9
tianlin0 <tianlin0@vip.qq.com>
2025-06-24 09:41:11 +0800

    commError

--- commerror/error.go
+++ commerror/error.go
@@ -2,6 +2,8 @@ package commerror
 
 import (
 	"errors"
+	"fmt"
+	"github.com/json-iterator/go"
 	"github.com/magic-lib/go-plat-utils/commerror/errpb"
 	"github.com/magic-lib/go-plat-utils/conf"
 	"github.com/samber/lo"
@@ -12,13 +14,17 @@ import (
 
 // CommError 通用错误信息
 type CommError interface {
-	Error() string
+	error
 	Code() int
+	Cause() error
+	Details() map[string]any
 }
 
 type commErr struct {
 	code    int
 	message string
+	cause   error
+	details map[string]any
 }
 
 // Error 错误信息返回，实现error接口
@@ -26,7 +32,31 @@ func (err *commErr) Error() string {
 	if err == nil {
 		return conf.EmptyString
 	}
-	return err.message
+	causeMsg := conf.EmptyString
+	if err.cause != nil {
+		causeMsg = err.cause.Error()
+	}
+
+	formattedErr := struct {
+		Code    int            `json:"code"`
+		Message string         `json:"message"`
+		Cause   string         `json:"cause"`
+		Details map[string]any `json:"details"`
+	}{
+		Code:    err.Code(),
+		Message: err.message,
+		Cause:   causeMsg,
+		Details: err.details,
+	}
+
+	errByte, errTemp := jsoniter.MarshalToString(formattedErr)
+	if errTemp != nil {
+		if causeMsg != "" {
+			return fmt.Sprintf("code=%d, message=%s, cause=%s", err.code, err.message, causeMsg)
+		}
+		return fmt.Sprintf("code=%d, message=%s", err.code, err.message)
+	}
+	return errByte
 }
 func (err *commErr) Code() int {
 	if err == nil {
@@ -35,34 +65,97 @@ func (err *commErr) Code() int {
 	return err.code
 }
 
+func (err *commErr) Cause() error {
+	return err.cause
+}
+
+func (err *commErr) Details() map[string]any {
+	if len(err.details) == 0 {
+		return make(map[string]any)
+	}
+	return err.details
+}
+
 // New 新建错误对象
-func New(msg string, code ...int) CommError {
+func New(msg string, opts ...Option) CommError {
 	err := &commErr{
 		code:    conf.DefaultErrorCode,
 		message: msg,
 	}
-	if len(code) > 0 {
-		err.code = code[0]
+	for _, opt := range opts {
+		opt(err)
 	}
 	return err
 }
 
-// Wrap 新增error Code
-func Wrap(err error, code ...int) CommError {
+// Option 错误构造选项
+type Option func(*commErr)
+
+// WithCode 设置错误码
+func WithCode(code int) Option {
+	return func(err *commErr) {
+		err.code = code
+	}
+}
+
+// WithCause 设置错误原因
+func WithCause(cause error) Option {
+	return func(err *commErr) {
+		err.cause = cause
+	}
+}
+
+// WithDetail 添加错误详情
+func WithDetail(key string, value any) Option {
+	return func(err *commErr) {
+		if len(err.details) == 0 {
+			err.details = make(map[string]any)
+		}
+		err.details[key] = value
+	}
+}
+
+// Wrap 包装现有错误
+func Wrap(err error, opts ...Option) CommError {
 	if err == nil {
 		return nil
 	}
-	errStr := err.Error()
-	var tempCode = conf.DefaultErrorCode
-	var errTemp CommError
-	if errors.As(err, &errTemp) {
-		tempCode = errTemp.Code()
-	} else {
-		if len(code) > 0 {
-			tempCode = code[0]
+
+	// 如果已经是CommError，直接添加选项
+	var commError CommError
+	if errors.As(err, &commError) {
+		newErr := &commErr{
+			code:    commError.Code(),
+			message: commError.Error(),
+			cause:   commError.Cause(),
+			details: commError.Details(),
 		}
+
+		for _, opt := range opts {
+			opt(newErr)
+		}
+
+		return newErr
 	}
-	return New(errStr, tempCode)
+
+	// 否则创建新的CommError
+	newErr := &commErr{
+		code:    conf.DefaultErrorCode,
+		message: err.Error(),
+		cause:   err,
+		details: make(map[string]any),
+	}
+
+	for _, opt := range opts {
+		opt(newErr)
+	}
+
+	return newErr
+}
+
+// WrapCode 兼容原有Wrap功能的快捷方式
+func WrapCode(err error, code int) CommError {
+	return Wrap(err, WithCode(code))
 }
 
 // GrpcError grpc错误信息
@@ -75,7 +168,7 @@ func GrpcError(code codes.Code, msg string, details ...CommError) error {
 		codesMsg = code.String()
 	} else {
 		// 自定义的值
-		oneError := New(msg, int(code))
+		oneError := New(msg, WithCode(int(code)))
 		if len(details) == 0 {
 			details = []CommError{}
 		}
@@ -108,10 +201,10 @@ func FromGrpcError(err error) (CommError, codes.Code) {
 				if codes.Code(detail.Code) == codes.OK { //如果是正确的，则直接返回nil
 					return nil, codes.OK
 				}
-				return New(detail.Message, int(detail.Code)), grpcStatus
+				return New(detail.Message, WithCode(int(detail.Code))), grpcStatus
 			}
 		}
-		return New(s.Message(), int(s.Code())), grpcStatus
+		return New(s.Message(), WithCode(int(s.Code()))), grpcStatus
 	}
 
 	var commErr CommError
