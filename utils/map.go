e6e89a9f3025280d3cbb45253a3fc24ecca52339
tianlin0 <tianlin0@vip.qq.com>
2025-08-24 03:55:07 +0800

    add MapFromKeyList

--- utils/map.go
+++ utils/map.go
@@ -10,7 +10,6 @@ import (
 	"github.com/tidwall/sjson"
 	"log"
 	"reflect"
-	"regexp"
 	"runtime"
 	"sort"
 	"strconv"
@@ -232,89 +231,6 @@ func DelKey(val map[string]any, fields []string) map[string]any {
 		delete(val, key)
 	}
 	return val
-}
-
-// ToMapFromKeyList "app.mm" : 1 ==> {"app":{"mm":1}}
-func ToMapFromKeyList(keyMapJsonObject any) map[string]any {
-	keyMapJson := conv.String(keyMapJsonObject)
-
-	keyMap := make(map[string]any)
-	_ = jsoniter.UnmarshalFromString(keyMapJson, &keyMap)
-	newMap := make(map[string]any)
-	for key, val := range keyMap {
-		keyList := strings.Split(key, ".")
-		toMapFromString(keyList, val, newMap)
-	}
-	return newMap
-}
-
-func toMapFromString(keyList []string, val any, oneMap map[string]any) {
-	var re, _ = regexp.Compile(`\[[0-9]+]$`)
-
-	for i, key := range keyList {
-		if key == "" {
-			continue
-		}
-		//如果要显示的数组
-		index := re.FindString(key)
-		realKey := key
-		isArray := false
-		isEnd := i == (len(keyList) - 1)
-		var indexNumber int
-		if index != "" {
-			realKey = strings.Replace(key, index, "", -1)
-			isArray = true
-			inStr := strings.ReplaceAll(index, "[", "")
-			inStr = strings.ReplaceAll(inStr, "]", "")
-			indexNumber, _ = strconv.Atoi(inStr)
-		}
-
-		if !isArray {
-			if isEnd {
-				if val != nil {
-					oneMap[realKey] = val
-				}
-				continue
-			}
-			if _, ok := oneMap[realKey]; !ok {
-				oneMap[realKey] = make(map[string]any)
-			}
-			tempMap := oneMap[realKey]
-			if one, ok := tempMap.(map[string]any); ok {
-				oneMap = one
-			}
-		} else {
-			if _, ok := oneMap[realKey]; !ok {
-				oneMap[realKey] = make([]any, 0)
-			}
-			if arr, ok := oneMap[realKey].([]any); ok {
-				if len(arr) <= indexNumber {
-					newArr := make([]any, indexNumber+1)
-					copy(newArr, arr)
-					arr = newArr
-				}
-
-				if isEnd {
-					arr[indexNumber] = val
-					oneMap[realKey] = arr
-					continue
-				}
-
-				var target map[string]any
-				if one, ok := arr[indexNumber].(map[string]any); ok {
-					target = one
-				} else {
-					arr[indexNumber] = make(map[string]any)
-					target, _ = arr[indexNumber].(map[string]any)
-				}
-
-				oneMap[realKey] = arr
-				oneMap = target
-			}
-		}
-
-	}
-	return
 }
 
 // GetStructInfoByTag converts golang struct field into slice string.
