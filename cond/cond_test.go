c0f32b3b525a6c28ef32e9d22230f11d8a2d58e9
tianlin0 <tianlin0@vip.qq.com>
2025-11-24 10:45:47 +0800

    fix: IsTimeEmpty 增加避免时区的影响

--- cond/cond_test.go
+++ cond/cond_test.go
@@ -13,18 +13,12 @@ func TestIsUUID(t *testing.T) {
 }
 func TestIsZero(t *testing.T) {
 	timeStr := "0001-01-01 00:00:00"
-
-	// 定义时间布局（必须与字符串格式完全匹配）
-	// 对应格式：年-月-日 时:分:秒
 	layout := "2006-01-02 15:04:05"
-
 	loc, err := time.LoadLocation("Asia/Shanghai")
 	if err != nil {
 		fmt.Printf("加载时区失败：%v\n", err)
 		return
 	}
-
-	// 解析字符串为 time.Time 类型
 	time2, _ := time.ParseInLocation(layout, timeStr, loc)
 	time3, _ := time.Parse(layout, timeStr)
 
