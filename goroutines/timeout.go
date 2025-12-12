22c893950d36ee3341786577163e0a1e96a87d78
tianlin0 <tianlin0@vip.qq.com>
2025-12-12 17:57:25 +0800

    fix: update lib

--- goroutines/timeout.go
+++ goroutines/timeout.go
@@ -6,16 +6,16 @@ import (
 )
 
 // GoAsyncWithTimeout 执行一个方法带过期时间
-func GoAsyncWithTimeout[T any](timeout time.Duration, fun func() (T, error)) (t T, e error) {
+func GoAsyncWithTimeout[T any](timeout time.Duration, fun func(params ...any) (T, error), params ...any) (t T, e error) {
 	result := make(chan T)
 	err := make(chan error)
 
 	// 启动一个 goroutine 来执行耗时操作
-	GoAsync(func(params ...interface{}) {
-		oneRet, oneErr := fun()
+	GoAsync(func(params ...any) {
+		oneRet, oneErr := fun(params...)
 		result <- oneRet
 		err <- oneErr
-	})
+	}, params...)
 
 	// 使用 select 语句来等待结果或超时
 	select {
