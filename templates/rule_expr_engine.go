f0147655d2b2b3a17573dbf89581a6c43d57b8bf
tianlin0 <tianlin0@vip.qq.com>
2026-05-04 21:53:03 +0800

    fix: RunString error

--- templates/rule_expr_engine.go
+++ templates/rule_expr_engine.go
@@ -2,6 +2,7 @@ package templates
 
 import (
 	"fmt"
+	"github.com/magic-lib/go-plat-utils/cond"
 	"github.com/magic-lib/go-plat-utils/conv"
 	"github.com/magic-lib/go-plat-utils/templates/ruleengine"
 )
@@ -59,6 +60,9 @@ func (e *RuleExprEngine) RunString(expr string, args any) (any, error) {
 	newWhenString := conv.String(newWhen)
 	retVal, err := ruleEngine.RunString(newWhenString, runStringArg)
 	if err != nil {
+		if cond.IsJson(newWhenString) { //如果错误是json格式，则直接返回即可，证明不是表达式
+			return newWhen, nil
+		}
 		// 有可能就没有变量，所以不需要去运行错误，比如一个字符串不加引号，就应该是正确的，如果有表达式，计算的话，就应该报错
 		fmt.Println("RuleExprEngine RunString expr:", expr, "return:", newWhenString, "args:", conv.String(args), "err:", err)
 		return newWhenString, err
